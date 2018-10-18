2d6d3501d345e3f2369c5499a638b529bf5b2007
cao <huntformm@gmail.com>
2018-10-18 10:31:27 +0800

    升级配置

--- seed.js
+++ seed.js
@@ -20,38 +20,7 @@ fibos.load("http", {
 
 
 fibos.load("net", {
-	"p2p-peer-address": [
-		"p2p.fibosutility.com:9870",
-		"p2p.xm.fo:10300",
-		"seed.fibos.icu:9870",
-		"fibos.smr123.com:7890",
-		"p2p.mainnet.fibos.me:80",
-		"seed-mainnet.fibscan.io:9103",
-		"p2p-mainnet.fibos123.com:9977",
-		"seed.fibos.rocks:10100",
-		"p2p-mainnet.ilovefibos.com:9876",
-		"p2p.otclook.com:9870",
-		"seed.mapleroad.top:9870",
-		"api.fibosgenesis.com:9870",
-		"seed.bitze.site:9870",
-		"va-p2p.fibos.io:9870",
-		"sl-p2p.fibos.io:9870",
-		"fibosiseos.xyz:9870",
-		"ln-p2p.fibos.io:9870",
-		"p2p.fibos.team:9876",
-		"seed.koalakoala.club:9870",
-		"ca-p2p.fibos.io:9870",
-		"p2p-mainnet.fobp.pro:9873",
-		"seed.loveparis.icu:9870",
-		"p2p.fometa.io:59877",
-		"seed.splo.top:9870",
-		"seed.fibospubg.top:9870",
-		"seed.franconofurd.top:9870",
-		"ppray.com:9870",
-		"p2p.fophoenix.com:9870",
-		"seed.fiboso.com:9965",
-		"fibos.qubitfund.com:9870"
-	],
+	"p2p-peer-address": config.p2p_peer_address,
 	"max-clients": 100,
 	"p2p-listen-endpoint": "0.0.0.0:9870"
 });
