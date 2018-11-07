0b5b340e72bcfe868c42ca96fdb2c115a910373f
cao <huntformm@gmail.com>
2018-11-08 00:53:01 +0800

    增加stop

---
+++ stopseed.js
@@ -0,0 +1,59 @@
+var fibos = require('fibos');
+var fs = require("fs");
+var config = require('./config');
+console.notice("start FIBOS seed node");
+
+
+fibos.config_dir = config.config_dir;
+fibos.data_dir = config.data_dir;
+
+console.notice("config_dir:", fibos.config_dir);
+console.notice("data_dir:", fibos.data_dir);
+
+
+fibos.load("http", {
+	"http-server-address": "0.0.0.0:8870",
+	"access-control-allow-origin": "*",
+	"http-validate-host": false,
+	"verbose-http-errors": true //打开报错
+});
+
+
+fibos.load("net", {
+	"p2p-peer-address": config.p2p_peer_address,
+	"max-clients": 100,
+	"p2p-listen-endpoint": "0.0.0.0:9870"
+});
+
+var chain_config = {
+	"contracts-console": true,
+	//'chain-state-db-size-mb': 8 * 1024,
+	// "delete-all-blocks": true
+};
+
+if (!fs.exists(fibos.data_dir) && !fs.exists(fibos.config_dir)) {
+	chain_config['genesis-json'] = "genesis.json";
+}
+
+
+fibos.load("producer", {
+	// 'enable-stale-production': true,
+	'max-transaction-time': 3000
+});
+
+fibos.load("emitter");
+
+var stop_block_num = 9210481 - 500;
+fibos.on('action', function(at) {
+	var now_num = at.block_num.toString();
+	console.log(now_num);
+	if (now_num == stop_block_num) {
+		fibos.stop();
+	}
+});
+
+fibos.load("chain", chain_config);
+fibos.load("chain_api");
+
+
+fibos.start();
