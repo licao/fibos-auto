c2d97cccd97cf65589eb82f320a6c02d0f868cad
cao <huntformm@gmail.com>
2018-11-07 15:58:27 +0800

    备份代码

--- backup.js
+++ backup.js
@@ -1,18 +1,44 @@
-var coroutine = require('coroutine');
-var config = require('./config');
+const coroutine = require('coroutine');
+const config = require('./config');
+const http = require("http");
 
 let p;
 
-
 function runSeed() {
-	endSeed();
 	p = process.start('fibos', ['seed.js']);
 }
 
 function endSeed() {
-	if (p) p.kill(15);
+	if (p) {
+		console.log('kill fibos');
+		p.kill(15);
+	}
+	console.log('sleep 1 s');
 	coroutine.sleep(1000);
 }
 
 
+runSeed();
+syncData();
 
+function syncData() {
+	console.log("start now ,waiting 10 s")
+	coroutine.sleep(10 * 1000);
+	const rep = http.post("http://127.0.0.1:8870/v1/chain/get_info", {
+		json: {}
+	});
+	const a = rep.json();
+
+	console.log("now head_block_num==> ",a.head_block_num);
+
+	endSeed();
+	console.log("tar  =====>");
+	process.run('tar', ['-zcvf', config.backup_dir + "/data_" + a.head_block_num + ".tar.gz", config.data_dir]);
+
+	console.log("restart   sync");
+	runSeed();
+
+	console.log("waiting 30 s");
+	coroutine.sleep(30 * 1000);
+	syncData()
+}
